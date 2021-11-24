2d9fae893990f9759a883f2264f76062ece0bb1a
Chrashtan <48963076+Chrashtan@users.noreply.github.com>
2021-11-24 22:48:50 +0100

    Canny Algorithmus hinzugefügt. Filter von ImgGauß geändert um Kanten besser zu detektieren. TODO: Eventuell Kontrast in ImgGauß erhöhen um Karte vom Tisch abzugrenzen.

--- cards.py
+++ cards.py
@@ -12,20 +12,6 @@ from enum import Enum
 
 # Properties of the card
 
-class CardProperties:
-    """Structure to store information about the cards of the camera image."""
-
-    def __init__(self):
-        self.frame = []  # Frame of card
-        self.corner = []  # Corner of card
-        self.width = 0  # Width of card
-        self.height = 0  # height of card
-        self.rank_img = []  # Image of card's rank
-        self.suit_img = []  # Image of card's suit
-        CardRanks.rank_name = "Unknown"  # Name of the rank of the card. Has to be determined
-        CardSuits.suit_name = "Unknown"  # Name of the suit of the card. Has to be determined
-
-
 class CardRanks(Enum):
     ACE = "ace"
     TWO = "two"
@@ -47,3 +33,18 @@ class CardSuits(Enum):
     CLUBS = "clubs"
     HEARTS = "hearts"
     SPADES = "spades"
+
+
+class CardProperties:
+    """Structure to store information about the cards of the camera image."""
+
+    def __init__(self):
+        self.frame = []  # Frame of card
+        self.corner = []  # Corner of card
+        self.width = 0  # Width of card
+        self.height = 0  # height of card
+        self.rank_img = []  # Image of card's rank
+        self.suit_img = []  # Image of card's suit
+        CardRanks.rank_name = "Unknown"  # Name of the rank of the card. Has to be determined
+        CardSuits.suit_name = "Unknown"  # Name of the suit of the card. Has to be determined
+
--- main.py
+++ main.py
@@ -11,25 +11,30 @@ from matplotlib import pyplot as plt
 import cards
 
 # TODO: Read from Camera -> Test with pictures loaded from Computer
-
-ImageGrayscale = cv.imread("PicturesOfCards/Aces_4.jpg", cv.IMREAD_GRAYSCALE)
-ImageGrayscaleResized = cv.resize(ImageGrayscale, dsize=(0, 0), fy=0.5, fx=0.5)
+ImageGrayscale = cv.imread("PicturesOfCards/Aces_2.jpg", cv.IMREAD_GRAYSCALE)
+ImageGrayscaleResized = cv.resize(ImageGrayscale, dsize=(0, 0), fy=0.50, fx=0.50)
 
 # Preprocess image
-ImgGauß = cv.GaussianBlur(ImageGrayscaleResized, (21, 21), 7)
+ImgGauß = cv.GaussianBlur(ImageGrayscaleResized, (11, 11), 0)
 ImgSmooth = cv.medianBlur(ImgGauß, 9)
 ret, ImageBinarised = cv.threshold(ImageGrayscaleResized, 0, 255, cv.THRESH_OTSU)
+
+# Process Image using Canny
+ImgCanny = cv.Canny(ImageGrayscaleResized, 30, 150)
+ImgEdged = cv.Canny(ImgGauß, 30, 150)
 
 # Find Contours
 CardContours, hierachyf = cv.findContours(ImageBinarised, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 # draw contour to image and display
-cv.drawContours(ImageGrayscaleResized, CardContours, -1, (0,255,0), 3)
+cv.drawContours(ImageGrayscaleResized, CardContours, -1, (0, 255, 0), 3)
 cv.imshow('Smooth', ImgSmooth)
 cv.imshow('Binary', ImageBinarised)
+cv.imshow('Canny', ImgCanny)
+cv.imshow('Canny blurred', ImgEdged)
 # Print Number of Contours found in the image
 print(len(CardContours))
 
-cv.imshow('Picture one', ImageGrayscaleResized)
+cv.imshow('Original Picture', ImageGrayscaleResized)
 cv.waitKey(0)
 cv.destroyAllWindows()
