2c2148781513e4559bc7dc9414b5382097cb5b1b
Chrashtan <48963076+Chrashtan@users.noreply.github.com>
2022-01-04 14:23:25 +0100

    Die main in 2 Dateien aufgeteilt. die main_single_pic.py ist für eingelesene Bilder (Testen). Die main_cam.py ruft ein Bild von der Webcam auf um ein Live Videofeed zu ermöglichen

---
+++ main_cam.py
@@ -0,0 +1,43 @@
+# ***********************************************
+# ***********************************************
+# **************** Main script ******************
+# ***********************************************
+# ***********************************************
+
+# ------ Import libaries -------
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import cards
+import processVideo as pV
+
+
+# ----- FOREVER LOOP -----
+# Generation of an captured object
+WebCam = cv.VideoCapture(0)
+
+# repeat the following lines as long as the Webcam is accessible
+while WebCam.isOpened():
+    # Reading a single image from the WebCam
+    Return, Image = WebCam.read()
+    # Check whether image was captured
+    if Return:
+        # Change the size of the image
+        ChangedImage = cv.resize(Image, dsize=(0, 0), fx=0.5, fy=0.5)
+
+        # Convert the webcam image from BGR to HSV color space
+        Image_hsv = cv.cvtColor(Image, cv.COLOR_BGR2HSV)
+
+        # Convert the webcam image from BGR to HSV color space
+        Image_GRAY = cv.cvtColor(Image, cv.COLOR_BGR2GRAY)
+
+        # Show the contents of image
+        cv.imshow('WebCam', Image)
+        cv.imshow('B', ChangedImage[:, :, 0])
+        cv.imshow('G', ChangedImage[:, :, 1])
+        cv.imshow('R', ChangedImage[:, :, 2])
+        if cv.waitKey(1) == ord('q'):
+            break
+
+# Close all windows
+cv.destroyAllWindows()
--- main_single_pic.py
+++ main_single_pic.py
@@ -11,13 +11,15 @@ from matplotlib import pyplot as plt
 import cards
 import processVideo as pV
 
+
+
+
+# ----- FOREVER LOOP -----
 # TODO: Read from Camera -> Test with pictures loaded from Computer
 # Read in Image resize it and convert to grayscale
 ImageOriginal = cv.imread("PicturesOfCards/Aces_4.jpg")     # Works best for a darker background
 ImageOriginalResized = cv.resize(ImageOriginal, dsize=(0, 0), fy=0.50, fx=0.50)
 
-
-# ----- FOREVER LOOP -----
 # TODO: ADD FOREVER LOOP WITH LIVE VIDEO FEED
 ListOfCardContours = []
 ListOfCards = []
